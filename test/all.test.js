bc5e71d674c8eead308b52a694a3a7a406b37379
overlookmotel <theoverlookmotel@gmail.com>
2014-11-17 03:14:03 +0000

    Empty skeleton

---
+++ test/all.test.js
@@ -0,0 +1,19 @@
+// --------------------
+// ejs-extra
+// Tests
+// --------------------
+
+// modules
+var chai = require('chai'),
+	expect = chai.expect,
+	promised = require('chai-as-promised'),
+	ejs-extra = require('../lib/');
+
+// init
+chai.use(promised);
+chai.config.includeStack = true;
+
+// tests
+
+describe('Tests', function() {
+});
